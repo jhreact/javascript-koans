afa8df3198f9522f06feed2a483afddeb32d78ee
jhreact <jhreact (at) users.noreply.github.com>
2014-05-16 23:16:20 -0700

    finds pizza - functional

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -40,8 +40,13 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      productsICanEat = _(products).filter(function(x) {
+        return (! x.containsNuts) && _(x.ingredients).all(function(ing) {
+          return ing !== "mushrooms"
+        });
+      });
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
